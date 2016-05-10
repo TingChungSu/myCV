243599a66447754241bcb31ecf607f60e63c03bc
TingChungSu <dingysu@gmail.com>
2016-05-10 16:16:27 +0800

    CV web

---
+++ js/cv.js
@@ -0,0 +1,47 @@
+$(document).ready(function($) {
+	/* scroll animation */
+	$("a.scroll-to-target").click(function() {
+		var url = $(this).attr("href");
+		var target = $(url);
+		$('html,body').animate({
+			scrollTop: target.offset().top}, 500
+		);
+		return false;
+	});   
+	
+	
+	/* peel animation */   
+	$("#peel").hover(function(){
+		// when mouse cover, stop acting animation and enlarge pic in 500ms
+		$("a img, a .peelBody", this).stop().animate({
+				width: 500,
+				height: 500
+			}, 500);
+
+	}, function(){	
+		// when mouse leave, stop acting animation and recovery pic in 200ms
+		$("a img, a .peelBody", this).stop().animate({
+				width: 50,
+				height: 50
+			}, 200);
+		timer = setTimeout(fish, 2000);
+	});
+
+	var speed = 1000;
+	function fish(){
+		// check mouse cover or not
+		var width = $("#peel a img").width();
+		if(width > 50)
+			return;
+		width = width > 0 ? 0 : 50;
+
+		$("#peel a img, #peel a .peelBody").animate({
+			width: width,
+			height: width
+		}, 700);
+		timer = setTimeout(fish, speed);
+	}
+
+	timer = setTimeout(fish, 2000);
+	
+});
